67e8a7aac28c4099756f2298ab8b59389db9dac1
AIZAWA-TOMO <116964452+TOMO-CHANN@users.noreply.github.com>
2023-06-28 22:11:26 +0530

    Update promote.js

--- Organs/commands/Group/promote.js
+++ Organs/commands/Group/promote.js
@@ -1,10 +1,10 @@
 require ('../../../settings')
 module.exports = {
 	name: "promote",
-	alias: ["pm"],
+	alias: ["xx"],
 	usage:`${prefa}promote @tag/quote`,
 	desc: "Promote Member To Admin",
-	react:"✅",
+	react:"🖤",
 	category: "Group",
 	start: async(client, m, { text, prefix, isBotAdmin,isAdmin,mentionByTag}) => {
 		if(!isAdmin) return client.sendMessage(m.from,{text:"This is admin only command"},{quoted:m})
