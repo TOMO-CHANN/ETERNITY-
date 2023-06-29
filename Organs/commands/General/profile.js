c13bd2a762608e64b1065a7711b94ba48cf02c1b
AIZAWA-TOMO <116964452+TOMO-CHANN@users.noreply.github.com>
2023-06-29 11:47:12 +0530

    Update profile.js

--- Organs/commands/General/profile.js
+++ Organs/commands/General/profile.js
@@ -96,7 +96,7 @@ let disec = m.sender.substring(3, 7)
 			try {
                     ppuser = await client.profilePictureUrl(m.sender, 'image')
                 } catch {
-                    ppuser = 'https://i.pinimg.com/564x/84/09/12/840912dd744e6662ab211b8070b5d84c.jpg'
+                    ppuser = 'https://wallpapercave.com/dwp1x/wp10709848.jpg'
                 }
 			}
 				
@@ -208,7 +208,7 @@ let disec = users.substring(3, 7)
 			try {
                     ppuser = await client.profilePictureUrl(users, 'image')
                 } catch {
-                    ppuser = 'https://i.pinimg.com/564x/84/09/12/840912dd744e6662ab211b8070b5d84c.jpg'
+                    ppuser = 'https://wallpapercave.com/dwp1x/wp10709848.jpg'
                 }
 			}
 				
